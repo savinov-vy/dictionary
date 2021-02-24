5ee0cc7a371cd5997bf27bf8885700d123bc8a48
savinov <savinov_vu@mail.ru>
2021-02-24 21:35:57 +0800

    Добавлен пример enum.

---
+++ src/main/java/ru/savinov/dictionary/theory/ENUM/TestWeekDaysENUM.java
@@ -0,0 +1,13 @@
+package ru.savinov.dictionary.theory.ENUM;
+
+public class TestWeekDaysENUM {
+    public static void main(String[] args) {
+        System.out.println(WeekDays.SUNDAY.getNumberOfDay());
+        System.out.println("установим для воскресенья значение 9");
+        WeekDays.SUNDAY.setNumberOfDay(9);
+        System.out.println(WeekDays.SUNDAY.getNumberOfDay());
+        System.out.println("получим значение enum из строки для среды");
+        WeekDays wednesday = WeekDays.valueOf("WEDNESDAY");
+        System.out.println(wednesday.getNumberOfDay());
+    }
+}
---
+++ src/main/java/ru/savinov/dictionary/theory/ENUM/WeekDays.java
@@ -0,0 +1,25 @@
+package ru.savinov.dictionary.theory.ENUM;
+// если объект типа WeekDays будет 1 # THURSDAY (остальные дни удалить) то это будет синглтон типа WeedDaysу.
+public enum WeekDays {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    int numberOfDay;
+
+    WeekDays(int numberOfDay) {
+        this.numberOfDay = numberOfDay;
+    }
+
+    public int getNumberOfDay() {
+        return numberOfDay;
+    }
+
+    public void setNumberOfDay(int numberOfDay) {
+        this.numberOfDay = numberOfDay;
+    }
+}
