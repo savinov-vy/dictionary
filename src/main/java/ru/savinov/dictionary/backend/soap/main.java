44d52f2db05b4f93c4be117b527e8386b95a2ede
Savinov <vy.savinov@gmail.com>
2022-04-23 09:28:23 +0800

    kafka docker image, settings producer and consumer

--- src/main/java/ru/savinov/dictionary/backend/soap/main.java
+++ src/main/java/ru/savinov/dictionary/backend/soap/main.java
@@ -2,6 +2,10 @@ package ru.savinov.dictionary.backend.soap;
 
 public class main {
     public static void main(String[] args) {
-
+        int i = 10;
+        while (i > 0) {
+            System.out.println("Привет" + i);
+            i--;
+        }
     }
 }
