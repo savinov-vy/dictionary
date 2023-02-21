24c0b8cbea791e4d52a28e3270acbba79ad26868
Savinov <savinov_vu@mail.ru>
2023-02-21 10:57:35 +0800

    add description subject and strategy

--- src/main/java/ru/savinov/dictionary/backend/patterns/observer/MainObserver.java
+++ src/main/java/ru/savinov/dictionary/backend/patterns/observer/MainObserver.java
@@ -4,8 +4,8 @@ import ru.savinov.dictionary.backend.patterns.observer.observers.Observer;
 import ru.savinov.dictionary.backend.patterns.observer.observers.ObserverImpl;
 import ru.savinov.dictionary.backend.patterns.observer.subjects.SubjectImpl;
 
-public class Main {
-    public static void mainObserverStart(String[] args) {
+public class MainObserver {
+    public static void main(String[] args) {
         SubjectImpl subject = new SubjectImpl();
         StateSubjectDto dto = new StateSubjectDto(4, "four number");
         Observer observer = new ObserverImpl(subject);
