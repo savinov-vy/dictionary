bffd53c236f370e813c921b22751e11c3e8d9ee8
Savinov <savinov_vu@mail.ru>
2024-02-21 15:12:21 +0800

    concurrent: reentrantLock

---
+++ src/main/java/ru/savinov/dictionary/backend/theoryCore/concurrent/ReentrantLockImpl.java
@@ -0,0 +1,38 @@
+package ru.savinov.dictionary.backend.theoryCore.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockImpl {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.submit(counter::countTo100_000);
+        executorService.submit(counter::countTo100_000);
+        executorService.shutdown();
+
+        executorService.awaitTermination(2, TimeUnit.HOURS);
+        System.out.println(counter.getCounted());
+    }
+}
+
+
+class Counter {
+    private int counted;
+    Lock lock = new ReentrantLock();
+
+    public void countTo100_000() {
+        lock.lock();
+        for (int i = 0; i < 100_000; i++) {
+            counted++;
+        }
+        lock.unlock();
+    }
+
+    public int getCounted() {
+        return counted;
+    }
+}
