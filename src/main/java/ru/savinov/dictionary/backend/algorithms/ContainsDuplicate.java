95b5aa18a181c91023611d0d33678e28d803410f
Savinov <savinov_vu@mail.ru>
2024-03-14 11:25:56 +0800

    algorithms: pivot integer

---
+++ src/main/java/ru/savinov/dictionary/backend/algorithms/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+package ru.savinov.dictionary.backend.algorithms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        if (nums.length < 2) {
+            return false;
+        }
+        Set<Integer> bucket = new HashSet<>();
+        bucket.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (bucket.contains(nums[i])) {
+                return true;
+            } else {
+                bucket.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
