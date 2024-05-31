f61277b81424c26d7e8414a7575205ca5075920e
Savinov <savinov_vu@mail.ru>
2024-05-31 11:34:38 +0800

    kafka docker-compose

---
+++ src/main/java/ru/savinov/dictionary/backend/algorithms/roadmap/TopKNumInList.java
@@ -0,0 +1,20 @@
+package ru.savinov.dictionary.backend.algorithms.roadmap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TopKNumInList {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 2, 3, 3, 3};
+        int[] ints = topKFrequent(nums, 2);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public static int[] topKFrequent(int[] nums, int k) {
+//  переделать
+        return null;
+    }
+}
