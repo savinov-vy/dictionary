b50b72f0d70907942f51ea49cd6beea3ea89692f
Savinov <savinov_vu@mail.ru>
2024-09-07 22:08:49 +0800

    task hh ru -> array blocking queue -> linked list

--- src/main/java/ru/savinov/dictionary/backend/algorithms/hhru/Main.java
+++ src/main/java/ru/savinov/dictionary/backend/algorithms/hhru/Main.java
@@ -1,15 +1,16 @@
 package ru.savinov.dictionary.backend.algorithms.hhru;
 
+import ru.savinov.dictionary.backend.algorithms.hhru.max_area.Data;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,56 +19,59 @@ public class Main {
     static Set<Integer> isChecked = new HashSet<>();
     static int[] valueByIndex;
     static Map<Integer, List<Integer>> indexesByValue = new HashMap<>();
-    static BlockingQueue<Island> toFindQueue = new ArrayBlockingQueue<>(5000);
-    static BlockingQueue<Island> neighboursQueue = new ArrayBlockingQueue<>(5000);
+    static Queue<Island> neighboursQueue = new LinkedList<>();
     static ExecutorService executorServiceProducer;
-    static ExecutorService executorServiceConsumer;
     static boolean isFounded = false;
+    static int targetIndex;
+    static long start;
 
     public static void main(String[] args) throws InterruptedException {
+        start = System.currentTimeMillis();
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        String[] s = line.split(" ");
-        valueByIndex = Arrays.stream(s)
-                .mapToInt(Integer::valueOf)
-                .toArray();
+        String line = Data.allDifferent; //in.nextLine();
+        String[] numbers = line.split(" ");
 
-        fillIndexesByValue(valueByIndex);
+        valueByIndex = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            valueByIndex[i] = Integer.parseInt(numbers[i]);
+        }
 
-        int halfAvailableProcessors = Runtime.getRuntime().availableProcessors() / 2;
-        executorServiceProducer = Executors.newFixedThreadPool(halfAvailableProcessors);
-        executorServiceConsumer = Executors.newFixedThreadPool(halfAvailableProcessors);
+        if (valueByIndex.length == 1) {
+            System.out.println(0);
+            return;
+        }
+        if (valueByIndex.length == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        targetIndex = valueByIndex.length - 1;
+
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        executorServiceProducer = Executors.newFixedThreadPool(availableProcessors);
 
         Main main = new Main();
 
-        executorServiceProducer.submit(main::fillIslandsQueue);
-        executorServiceConsumer.submit(main::foundedTarget);
+        main.fillIndexesByValue();
+        executorServiceProducer.submit(main::foundTarget);
         executorServiceProducer.shutdown();
-        executorServiceConsumer.shutdown();
     }
 
-    private void fillIslandsQueue() {
-        try {
+    private void foundTarget() {
             Island current = new Island(valueByIndex[0], 0, 0);
             neighboursQueue.add(current);
-            toFindQueue.add(current);
             isChecked.add(current.getIndex());
             while (!isFounded) {
-                Island island = neighboursQueue.take();
-                if (island != null) {
-                    List<Island> neighboursIslands = getNeighboursIslands(island);
-                    for (Island neighbor : neighboursIslands) {
-                        if (!isChecked.contains(neighbor.getIndex())) {
-                            neighboursQueue.put(neighbor);
-                            toFindQueue.put(neighbor);
-                            isChecked.add(neighbor.getIndex());
-                        }
+                Island island = neighboursQueue.poll();
+                List<Island> neighboursIslands = getNeighboursIslands(island);
+                for (Island neighbor : neighboursIslands) {
+                    if (!isChecked.contains(neighbor.getIndex())) {
+                        neighboursQueue.add(neighbor);
+                        isChecked.add(neighbor.getIndex());
                     }
                 }
             }
-        } catch (InterruptedException e) {}
     }
-
 
     private List<Island> getNeighboursIslands(Island island) {
 
@@ -95,36 +99,36 @@ public class Main {
             neighbours.addAll(indexes);
         }
         List<Island> islands = new ArrayList<>();
-        for (Integer n : neighbours) {
-            if (isChecked.contains(n)) {
+        for (Integer indexNeighbour : neighbours) {
+            if (isChecked.contains(indexNeighbour)) {
                 continue;
             }
-            islands.add(new Island(valueByIndex[n], n, island.getLevel() + 1));
+            int currentLevel = island.getLevel();
+            boolean check = check(currentLevel, indexNeighbour);
+            if (check) {
+                break;
+            }
+            islands.add(new Island(valueByIndex[indexNeighbour], indexNeighbour, currentLevel + 1));
+            System.out.println("Memory 2: " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())) / 1048576); //!!!!!!!!!
         }
         return islands;
     }
 
-    private void foundedTarget() {
-        try {
-            int targetIndex = valueByIndex.length - 1;
-            while (!isFounded) {
-                Island current = toFindQueue.take();
-                if (current != null) {
-                    if (current.getIndex() == targetIndex) {
-                        System.out.println(current.getLevel());
-                        isFounded = true;
-                        executorServiceProducer.shutdownNow();
-                        executorServiceConsumer.shutdownNow();
-                    }
-                }
-
+    private boolean check(int currentLevel, int indexNeighbour) {
+        if (!isChecked.contains(indexNeighbour)) {
+            if (indexNeighbour == targetIndex) {
+                System.out.println(currentLevel + 1);
+                isFounded = true;
+                executorServiceProducer.shutdownNow();
+                System.out.println(System.currentTimeMillis() - start);
             }
-        } catch (InterruptedException e) {}
+        }
+        return isFounded;
     }
 
-    private static void fillIndexesByValue(int[] s) {
-        for (int i = 0; i < s.length; i++) {
-            int current = s[i];
+    private void fillIndexesByValue() {
+        for (int i = 0; i < valueByIndex.length; i++) {
+            int current = valueByIndex[i];
             if (!indexesByValue.containsKey(current)) {
                 List<Integer> indexes = new ArrayList<>();
                 indexes.add(i);
